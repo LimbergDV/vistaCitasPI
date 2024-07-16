bf72b79672eeaf228b08be3c4f9c9bd90a507a19
Alexis Guzman <spartanthelexvro@gmail.com>
2024-07-15 23:01:42 -0600

    Modulo gestion de ventas 90%, falta generar PDF

--- src/components/Forms/FormCobroRecepcionista.jsx
+++ src/components/Forms/FormCobroRecepcionista.jsx
@@ -1,165 +1,344 @@
 import React from "react";
 import { FaCircleInfo } from "react-icons/fa6";
-import '../styles/formCobroRecepcionista.css'
+import "../styles/formCobroRecepcionista.css";
 import TableCotizacionUser from "../Tables/tableCotizacionUser";
 import TablePagoRecepcionista from "../Tables/tablePagoRecepcionista";
+import { useState } from "react";
+import { fetchData } from "../../fetchData";
+import Swal from "sweetalert2";
 
-function FormCobroRecepcionista(){
-    return(
-        <>
-        <div className="customPrincipalContent">
-  <div className="customFormLogin">
-    <form>
-      <h3 className="customSecondText">
-        <FaCircleInfo className="customInfoIcon" />
-        Sobre el paciente
-      </h3>
-      <div className="customSobrePaciente">
-        <div className="customInputsPrincipal">
-          <div>
-            <label htmlFor="nombre" className="customLabelName">
-              Nombre:
-            </label>
-            <input
-              name="nombre"
-              type="text"
-              placeholder="Ingrese su nombre"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="apellidoP" className="customLastName">
-              Apellido Paterno:
-            </label>
-            <input
-              name="apellidoP"
-              type="text"
-              placeholder="Ingrese su apellido paterno"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="apellidoM" className="customLastName2">
-              Apellido Materno:
-            </label>
-            <input
-              name="apellidoM"
-              type="text"
-              placeholder="Ingrese su apellido materno"
-              required
-            />
-          </div>
-        </div>
-        <div className="customInputsPrincipal">
-          <div>
-            <label htmlFor="fecha_nacimiento" className="customBirthDate">
-              Fecha de nacimiento:
-            </label>
-            <input
-              name="fecha_nacimiento"
-              id="date"
-              type="date"
-              placeholder="Ingrese su fecha de nacimiento"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="id_genero" className="customGender">
-              Género:
-            </label>
-            <select
-              name="id_genero"
-              className="customSelect"
-            >
-            </select>
-          </div>
-          <div>
-            <label htmlFor="telefono" className="customPhoneNumber">
-              Teléfono:
-            </label>
-            <input
-              name="telefono"
-              type="text"
-              placeholder="Ingrese su teléfono"
-              maxLength={10}
-              required
-            />
-          </div>
-        </div>
-        <h3 className="customDireccion">Dirección</h3>
-        <div className="customInputsPrincipal">
-          <div>
-            <label htmlFor="calle" className="customStreet">
-              Calle:
-            </label>
-            <input
-              name="calle"
-              type="text"
-              placeholder="Ingrese su calle"
-              required
-            />
-            <label htmlFor="numero" className="customNumberHouse">
-              Número:
-            </label>
-            <input
-              name="numero"
-              type="number"
-              placeholder="Ingrese número de su casa"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="codigo_postal" className="customPostalCode">
-              Código Postal:
-            </label>
-            <input
-              name="codigo_postal"
-              type="number"
-              placeholder="Ingrese el código postal"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="id_colonia" className="customNeighborhood">
-              Colonia
-            </label>
-            <select
-              name="id_colonia"
-              className="customSelect"
-            >
-            </select>
-          </div>
+// Métodos GET usados en la página
+const token = import.meta.env.VITE_TOKEN;
+const url = import.meta.env.VITE_URL_BASE;
+
+const options = {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+};
+const generos = fetchData(`${url}/genders/getAll/`, options);
+const colonias = fetchData(`${url}/colonies/getAll/`, options);
+
+function FormCobroRecepcionista() {
+  const [total, setTotal] = useState(null);
+  const [totalData, setTotalData] = useState({ subTotal: "0", total: "0" });
+
+  const handleTotal = (t) => {
+    setTotal(t);
+    setTotalData({ subTotal: total, total: total });
+  };
+
+  // Construcción del modelo de datos para insertar una cita
+  const [formPaciente, setFormPaciente] = useState({
+    nombre: "",
+    apellidoP: "",
+    apellidoM: "",
+    fecha_nacimiento: "",
+    id_genero: "",
+    telefono: "",
+  });
+
+  const [formDireccion, setFormDireccion] = useState({
+    calle: "",
+    id_colonia: "",
+    numero: "",
+    codigo_postal: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (
+      [
+        "nombre",
+        "apellidoP",
+        "apellidoM",
+        "fecha_nacimiento",
+        "id_genero",
+        "telefono",
+      ].includes(name)
+    ) {
+      setFormPaciente((prevState) => ({ ...prevState, [name]: value }));
+    } else if (
+      ["calle", "numero", "codigo_postal", "id_colonia"].includes(name)
+    ) {
+      setFormDireccion((prevState) => ({ ...prevState, [name]: value }));
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // POST en la tabla direccion
+    console.log(formDireccion);
+    if (formDireccion.id_colonia === "") {
+      formDireccion.id_colonia = 1;
+    }
+    const response = await fetch(`${url}/direcciones/add/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formDireccion),
+    });
+    const resDireccion = await response.json();
+    console.log(resDireccion);
+
+    // POST en la tabla pacientes
+    if (formPaciente.id_genero === "") {
+      formPaciente.id_genero = 1;
+    }
+    const respon = await fetch(`${url}/patients/add/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...formPaciente,
+        id_direccion: resDireccion.id_direccion,
+      }),
+    });
+    const resPacientes = await respon.json();
+    console.log(resPacientes);
+
+    //POST en la tabla ventas
+    const respo = await fetch(`${url}/sales/add/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        monto: total,
+        id_paciente: resPacientes.id_paciente,
+      }),
+    });
+    if (respo.ok) {
+      const options = {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const response = await fetch(
+        `${url}/price/deleteAll/2`,
+        options
+      );
+
+      if (response.ok) {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Cobro registrado",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="customPrincipalContent">
+        <div className="customFormLogin">
+          <form onSubmit={handleSubmit}>
+            <h3 className="secondText">
+              <FaCircleInfo className="infoIcon" />
+              Sobre el paciente
+            </h3>
+            <div className="sobre-paciente">
+              <div className="inputsPrincipal">
+                <div>
+                  <label htmlFor="nombre" className="labelName">
+                    Nombre:
+                  </label>
+                  <input
+                    name="nombre"
+                    type="text"
+                    placeholder="Ingrese su nombre"
+                    required
+                    value={formPaciente.nombre}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="apellidoP" className="lastName">
+                    Apellido Paterno:
+                  </label>
+                  <input
+                    name="apellidoP"
+                    type="text"
+                    placeholder="Ingrese su apellido paterno"
+                    required
+                    value={formPaciente.apellidoP}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="apellidoM" className="lastName2">
+                    Apellido Materno:
+                  </label>
+                  <input
+                    name="apellidoM"
+                    type="text"
+                    placeholder="Ingrese su apellido materno"
+                    required
+                    value={formPaciente.apellidoM}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="inputsPrincipal">
+                <div>
+                  <label htmlFor="fecha_nacimiento" className="birthDate">
+                    Fecha de nacimiento:
+                  </label>
+                  <input
+                    name="fecha_nacimiento"
+                    id="date"
+                    type="date"
+                    placeholder="Ingrese su fecha de nacimiento"
+                    required
+                    value={formPaciente.fecha_nacimiento}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="id_genero" className="gender">
+                    Género:
+                  </label>
+                  <select
+                    name="id_genero"
+                    className="select"
+                    value={formPaciente.id_genero}
+                    onChange={handleChange}
+                  >
+                    {generos.read()?.map((genero) => (
+                      <option key={genero.id_genero} value={genero.id_genero}>
+                        {genero.nombre_genero}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="telefono" className="phoneNumber">
+                    Teléfono:
+                  </label>
+                  <input
+                    name="telefono"
+                    type="text"
+                    placeholder="Ingrese su teléfono"
+                    maxLength={10}
+                    required
+                    value={formPaciente.telefono}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <h3 className="direccion">Dirección</h3>
+              <div className="inputsPrincipal">
+                <div>
+                  <label htmlFor="calle" className="street">
+                    Calle:
+                  </label>
+                  <input
+                    name="calle"
+                    type="text"
+                    placeholder="Ingrese su calle"
+                    required
+                    value={formDireccion.calle}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="numero" className="numberHouse">
+                    Número:
+                  </label>
+                  <input
+                    name="numero"
+                    type="number"
+                    placeholder="Ingrese número de su casa"
+                    required
+                    value={formDireccion.numero}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="codigo_postal" className="postalCode">
+                    Código Postal:
+                  </label>
+                  <input
+                    name="codigo_postal"
+                    type="number"
+                    placeholder="Ingrese el código postal"
+                    required
+                    value={formDireccion.codigo_postal}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="id_colonia" className="neighborhood">
+                    Colonia
+                  </label>
+                  <select
+                    name="id_colonia"
+                    className="select"
+                    value={formDireccion.id_colonia}
+                    onChange={handleChange}
+                  >
+                    {colonias.read()?.map((colonia) => (
+                      <option
+                        key={colonia.id_colonia}
+                        value={colonia.id_colonia}
+                      >
+                        {colonia.nombre}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            </div>
+            <h3 className="customSecondText">
+              <FaCircleInfo className="customInfoIcon" />
+              Sobre el pago
+            </h3>
+            <div className="customCotizacionContainer">
+              <div className="customCotizacionSearch">
+                <h2>Análisis escogidos</h2>
+                <TablePagoRecepcionista onTotal={handleTotal} />
+                <div className="form-cotizacion-inputs">
+                  <div className="form-cotizacion-input-group">
+                    <label>Subtotal:</label>
+                    <input
+                      type="text"
+                      placeholder="Subtotal"
+                      disabled
+                      value={totalData.subTotal}
+                    />
+                  </div>
+                  <div className="form-cotizacion-input-group">
+                    <label>Total:</label>
+                    <input
+                      type="text"
+                      placeholder="Total"
+                      disabled
+                      value={totalData.total}
+                    />
+                  </div>
+                  <button className="form-cotizacion-button" type="submit">
+                    Cobrar
+                  </button>
+                </div>
+                <br />
+                <br />
+              </div>
+            </div>
+          </form>
         </div>
       </div>
-      <h3 className="customSecondText">
-        <FaCircleInfo className="customInfoIcon" />
-        Sobre el pago
-      </h3>
-      <div className="customCotizacionContainer">
-  <div className="customCotizacionSearch">
-    <h2>Análisis escogidos</h2>
-    <TablePagoRecepcionista />
-    <div className="customCotizacionInputs">
-      <div className="customCotizacionInputGroup">
-        <label>Subtotal:</label>
-        <input type="text" placeholder="Subtotal" disabled />
-      </div>
-      <div className="customCotizacionInputGroup">
-        <label>Total:</label>
-        <input type="text" placeholder="Total" disabled />
-      </div>
-      <button className="customCotizacionButton">
-        Agendar Cita
-      </button>
-    </div>
-    <br /><br />
-  </div>
-</div>
-    </form>
-  </div>
-</div>
     </>
-    )
+  );
 }
 
-export default FormCobroRecepcionista
+export default FormCobroRecepcionista;
--- src/components/Tables/tableHistorialPagos.jsx
+++ src/components/Tables/tableHistorialPagos.jsx
@@ -1,42 +1,95 @@
-import React, { useState } from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import Button from '@mui/material/Button';
+import React, { useState, useEffect } from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
 import { MdLocalPrintshop } from "react-icons/md";
 
+const token = import.meta.env.VITE_TOKEN;
+const url = import.meta.env.VITE_URL_BASE;
 
+function formatDateTime(dateTime) {
+   // Crear un objeto Date a partir de la cadena de fecha
+   const date = new Date(dateTime);
+
+   // Obtener los componentes de la fecha
+   const day = String(date.getUTCDate()).padStart(2, '0');
+   const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Los meses van de 0 a 11
+   const year = date.getUTCFullYear();
+   
+   // Obtener los componentes de la hora
+   const hour = String(date.getUTCHours()).padStart(2, '0');
+   const minute = String(date.getUTCMinutes()).padStart(2, '0');
+   const second = String(date.getUTCSeconds()).padStart(2, '0');
+
+   // Formatear la fecha y hora en el nuevo formato
+   const formattedDate = `${day}/${month}/${year} -- ${hour}:${minute}:${second}`;
+
+   return formattedDate;
+}
 
 const columns = [
-  { id: 'paciente', label: 'Paciente', minWidth: 170 },
-  { id: 'monto', label: 'Monto', minWidth: 170 },
-  { id: 'fechaPago', label: 'Fecha de pago', minWidth: 170},
+  { id: "paciente", label: "Paciente", minWidth: 170 },
+  { id: "monto", label: "Monto", minWidth: 170 },
+  { id: "fechaPago", label: "Fecha de pago", minWidth: 170 },
 ];
-const initialRows = [
-  { id: 1, nombre: 'Limberg Del Valle Vargas', monto: "$ "+"150", fechaPago: "23/07/2024"},
-];
-export default function TableHistorialPagos() {
-  const [rows, setRows] = useState(initialRows);
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
 
+export default function TableHistorialPagos() {
+  const [rows, setRows] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState();
+
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          `${url}/sales/getAll/`,
+          options
+        );
+        const data = await response.json();
+        console.log("Fetched data:", data); // Log the fetched data for debugging
+        if (Array.isArray(data)) {
+          setRows(data);
+          setRowsPerPage(data.length);
+        } else {
+          console.error("Data fetched is not an array:", data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    getData();
+  }, []);
 
   return (
-    <Paper sx={{ width: '70%', overflow: 'hidden', backgroundColor: '#C2BEBE', }}>
+    <Paper
+      sx={{ width: "70%", overflow: "hidden", backgroundColor: "#C2BEBE" }}
+    >
       <TableContainer>
         <Table stickyHeader aria-label="sticky table">
-          <TableHead >
+          <TableHead>
             <TableRow>
               {columns.map((column) => (
                 <TableCell
                   key={column.id}
                   align={column.align}
-                  style={{ minWidth: column.minWidth, backgroundColor: '#C2BEBE' }}
+                  style={{
+                    minWidth: column.minWidth,
+                    backgroundColor: "#C2BEBE",
+                  }}
                 >
                   {column.label}
                 </TableCell>
@@ -46,12 +99,12 @@ export default function TableHistorialPagos() {
           <TableBody>
             {rows
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
+              .map((data) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                    <TableCell>{row.nombre}</TableCell>
-                    <TableCell>{row.monto}</TableCell>
-                    <TableCell>{row.fechaPago}</TableCell>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={data.id_pago}>
+                    <TableCell>{`${data.nombre} ${data.apellidoP} ${data.apellidoM}`}</TableCell>
+                    <TableCell>{data.monto}</TableCell>
+                    <TableCell>{formatDateTime(data.fecha_pago)}</TableCell>
                   </TableRow>
                 );
               })}
