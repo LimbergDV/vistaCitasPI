4e4f829b24ce0774cfd7048372d140a5e23e60ee
Alexis Guzman <spartanthelexvro@gmail.com>
2024-07-24 16:24:02 -0600

    Añadiendo logica a NavBar

---
+++ src/components/NavBar/navBarAdmin.jsx
@@ -0,0 +1,159 @@
+import logo from '../images/WhatsApp Image 2024-06-12 at 06.38-Photoroom.jpg';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import HomeIcon from '@mui/icons-material/Home';
+import { SlChemistry } from "react-icons/sl";
+import { IoIosPerson } from "react-icons/io";
+import { TfiWrite } from "react-icons/tfi";
+import { IoPersonCircle } from "react-icons/io5";
+
+const pages = [
+  { name: 'Comenzar Consulta', icon: <TfiWrite /> },
+  { name: 'Iniciar Sesión', icon: <IoPersonCircle />}
+];
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ height: '90px', backgroundColor: '#20215a' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box
+            component="img"
+            src={logo}
+            alt="logo"
+            sx={{
+              ml: 2,
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              height: 60,
+            }}
+          />
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Box sx={{ flexGrow: 50, display: { xs: 'flex', md: 'none' }, justifyContent: 'center' }}>
+            <Box
+              component="img"
+              src={logo}
+              alt="logo"
+              sx={{
+                height: 40,
+              }}
+            />
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center', marginTop: '12px' }}>
+            {pages.map((page) => (
+              <Button
+                key={page.name}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'flex', alignItems: 'right', mx: 2 }}
+                startIcon={page.icon}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default NavBar;
--- src/components/Pages/pageAdminstrarPerfil.jsx
+++ src/components/Pages/pageAdminstrarPerfil.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import NavBar from "../NavBar/navBar";
+import NavBarRecepcionista from "../NavBar/navBarRecepcionista";
+import NavBarAdmin from "../NavBar/navBarAdmin";
 import FormAdministrarPerfil from "../Forms/FormAdministrarPerfil";
 
 function PageAdministrarPerfil(){
+    const id_rol = localStorage.getItem("id_rol");
+
     return(
         <>
-        <NavBar/>
+        {id_rol == 1? <NavBar/> : id_rol == 2? <NavBarRecepcionista/> : <NavBarAdmin /> }
         <br /> <br />
         <FormAdministrarPerfil/>
         </>
